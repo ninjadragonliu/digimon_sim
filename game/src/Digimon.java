0930a47acecbcbe2633d60b1aa6a65122bc4f896
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-10-15 14:13:29 -0400

    updated Digimon class

--- game/src/Digimon.java
+++ game/src/Digimon.java
@@ -1,2 +1,11 @@
-public class Digimon {
+public abstract class Digimon {
+
+    private String name;
+    private int hp;
+    private int attack;
+    private int defense;
+    private int speed;
+
+    private int level;
+    private int exp;
 }
