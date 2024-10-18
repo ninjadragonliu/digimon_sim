07125671375134fccdaf99b3b026a2fb67f0e2a7
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-10-18 08:54:56 -0400

    updated Gabumon class

--- game/src/digimons/Gabumon.java
+++ game/src/digimons/Gabumon.java
@@ -1,4 +1,13 @@
 package digimons;
 
-public class Gabumon {
+public class Gabumon extends Digimon{
+
+    public Gabumon() {
+        super("Gabumon", DigimonTypes.DATA, 100, 25, 35, 20);
+    }
+
+    @Override
+    public Digimon Digivolve() {
+        return null;
+    }
 }
