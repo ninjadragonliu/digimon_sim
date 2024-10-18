ad5e21aeab195c46b0af2835a8d830b53d9f5511
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-10-18 08:55:30 -0400

    updated Garurumon class

--- game/src/digimons/Garurumon.java
+++ game/src/digimons/Garurumon.java
@@ -1,4 +1,12 @@
 package digimons;
 
-public class Garurumon {
+public class Garurumon extends Digimon{
+    public Garurumon() {
+        super("Garurumon", DigimonTypes.DATA, 125, 45, 45, 30);
+    }
+
+    @Override
+    public Digimon Digivolve() {
+        return null;
+    }
 }
