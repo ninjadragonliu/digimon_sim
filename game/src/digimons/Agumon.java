1887b1a04ee44fa4253bfe69f1b21b1ac2b0462e
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-10-18 08:48:44 -0400

    updated Agumon class

--- game/src/digimons/Agumon.java
+++ game/src/digimons/Agumon.java
@@ -1,4 +1,12 @@
 package digimons;
 
-public class Agumon {
+public class Agumon extends Digimon{
+    public Agumon() {
+        super("Agumon", DigimonTypes.VACCINE, 100, 30, 25, 25);
+    }
+
+    @Override
+    public Digimon Digivolve() {
+        return null;
+    }
 }
